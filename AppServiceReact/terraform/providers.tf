050d84a0afcf615ec1c785e00a35379f46cfc505
Arik Bidny <arikbidny@gmail.com>
2022-11-29 17:23:46 +0200

    added terraform scripts and simple workflow

---
+++ AppServiceReact/terraform/providers.tf
@@ -0,0 +1,22 @@
+provider "azurerm" {
+  features {}
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.23.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name = "mtc-tfstates"
+    storage_account_name = "mtctfstates"
+    container_name = "terraformstate"
+    key = "terraformstate.tfstate"
+  }
+}
+
+
+# storage account = mtctfstates
+# container = terraformstate
